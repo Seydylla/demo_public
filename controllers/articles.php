284e8976b28bd89b17ad26a05c5359fbd3155c61
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-07-19 02:18:28 +0500

    1.1.3

--- controllers/articles.php
+++ controllers/articles.php
@@ -1,3 +1,10 @@
 <?php
 
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+$articles = [];
+
+$articles = $db->query('select * from articles')->fetchAll();
+
 require 'views/articles.view.php';
